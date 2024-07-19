99caed152f07df11cd84a1fe9b48fc4cbff4bce2
aleskutepov <ales.kutepov@gmail.com>
2024-07-19 17:23:51 +0300

    Added new types to validate topic json structure

--- psr.py
+++ psr.py
@@ -1,10 +1,27 @@
 import json
 
-type json_card_structure = dict[str, str | int]
+type question = str
+type answer = str
+type priority = int
+
+
+class JsonTopicKey(str):
+    ACCEPTABLE_KEYS = {"Priority", "Question", "Answer"}
+
+    def __new__(cls, *args, **kw):
+        if args[0] not in cls.ACCEPTABLE_KEYS:
+            raise ValueError(f"JSON Topic key <{args[0]}> is unexpected")
+        return str.__new__(cls, *args, **kw)
+
+
+type json_card_structure = dict[JsonTopicKey, question | answer | priority]
 type json_topic_structure = list[json_card_structure]
+
+DEFAULT_ENCODING = "utf-8"
+PRIORITY_PATH = "priority.json"
 
 
 def read_topic(path: str) -> json_topic_structure:
-    with open(path, "r", encoding="utf-8") as fstream:
+    with open(path, "r", encoding=DEFAULT_ENCODING) as fstream:
         json_topic: json_topic_structure = json.load(fstream)  # TODO validate
     return json_topic
