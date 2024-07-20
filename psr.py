f0462f81571e304e1f8dd6ead0b75d384db50ec7
aleskutepov <ales.kutepov@gmail.com>
2024-07-20 13:13:15 +0300

    added json structure validation

--- psr.py
+++ psr.py
@@ -1,20 +1,12 @@
 import json
 
+type json_card_key = str
 type question = str
 type answer = str
 type priority = int
+type json_card_structure = dict[json_card_key, question | answer | priority]
+JSON_CARD_STRUCTURE = {"Priority": int, "Question": str, "Answer": str}
 
-
-class JsonTopicKey(str):
-    ACCEPTABLE_KEYS = {"Priority", "Question", "Answer"}
-
-    def __new__(cls, *args, **kw):
-        if args[0] not in cls.ACCEPTABLE_KEYS:
-            raise ValueError(f"JSON Topic key <{args[0]}> is unexpected")
-        return str.__new__(cls, *args, **kw)
-
-
-type json_card_structure = dict[JsonTopicKey, question | answer | priority]
 type json_topic_structure = list[json_card_structure]
 
 DEFAULT_ENCODING = "utf-8"
@@ -23,5 +15,10 @@ PRIORITY_PATH = "priority.json"
 
 def read_topic(path: str) -> json_topic_structure:
     with open(path, "r", encoding=DEFAULT_ENCODING) as fstream:
-        json_topic: json_topic_structure = json.load(fstream)  # TODO validate
+        json_topic: json_topic_structure = json.load(fstream)
+        for card in json_topic:
+            for key, value in card.items():
+                assert key in JSON_CARD_STRUCTURE
+                assert isinstance(value, JSON_CARD_STRUCTURE[key])
+
     return json_topic
